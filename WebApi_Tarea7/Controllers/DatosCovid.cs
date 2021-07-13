f81934ad1a04ccd0e0b8d4d3c9ed8191e2f233cd
Jefferson Payano <76220445+Jeffersonp4@users.noreply.github.com>
2021-07-12 22:33:15 -0400

    Api_Base

---
+++ WebApi_Tarea7/Controllers/DatosCovid.cs
@@ -0,0 +1,106 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Tarea7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatosCovid : ControllerBase
+    {
+
+        private string Conexion = @"Server=localhost; uid=root; pwd=mysql; Database=tareacovid";
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            IEnumerable<Models.pacientes> lista = null;
+
+            using(var db = new MySqlConnection(Conexion))
+            {
+                var sql = "select id,cedula,nombre,apellido,telefono,fecha_nacimiento from pacientes";
+
+                lista = db.Query<Models.pacientes>(sql);
+            }
+
+            return Ok(lista);
+        }
+
+        [HttpPost]
+
+        public IActionResult Insert(Models.pacientes pacientes)
+        {
+            int result = 0;
+
+            using(var db = new MySqlConnection(Conexion))
+            {
+                var sql = "insert into pacientes(cedula,nombre,apellido,telefono,fecha_nacimiento)" +
+                    "values(@cedula,@nombre,@apellido,@telefono,@fecha_nacimiento)";
+
+                result = db.Execute(sql, pacientes);
+            }
+
+            if(result == 1)
+            {
+                return Ok("201 CREATED " + result);
+            }
+            else
+            {
+                return Ok("Error: 415 UNSUPPORTED MEDIA TYPE " + result);
+            }
+        }
+
+        [HttpPut]
+
+        public IActionResult Edit(Models.pacientes pacientes)
+        {
+            int result = 0;
+
+            using (var db = new MySqlConnection(Conexion))
+            {
+                var sql = "UPDATE pacientes SET cedula=@cedula, nombre=@nombre, apellido=@apellido,telefono=@telefono,fecha_nacimiento=@fecha_nacimiento" +
+                    " WHERE id=@id";
+
+                result = db.Execute(sql, pacientes);
+            }
+
+            if (result == 1)
+            {
+                return Ok("201 CREATED " + result);
+            }
+            else
+            {
+                return Ok("Error: 415 UNSUPPORTED MEDIA TYPE " + result);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Models.pacientes pacientes)
+        {
+            int result = 0;
+            using(var db = new MySqlConnection(Conexion))
+            {
+                var sql = "delete from pacientes where id=@id";
+
+                result = db.Execute(sql, pacientes);
+                
+            }
+
+            if (result == 1)
+            {
+                return Ok("201 CREATED " + result);
+            }
+            else
+            {
+                return Ok("Error: 415 UNSUPPORTED MEDIA TYPE " + result);
+            }
+        }
+
+    }
+}
