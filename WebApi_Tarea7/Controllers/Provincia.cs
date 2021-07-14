37ec95a5d38bfca5c5a51bd5eed943c9e5da6837
juniorulloa23 <59671640+juniorulloa23@users.noreply.github.com>
2021-07-14 13:04:07 -0400

    Provincias
    Added CRUD for Provincia table.
    
    Note: Get method is getting Pacientes by Provincia

---
+++ WebApi_Tarea7/Controllers/Provincia.cs
@@ -0,0 +1,139 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Tarea7.Models.ResponseApi;
+
+namespace WebApi_Tarea7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Provincia
+    {
+        Response Respuesta = new Response();
+        private string Conexion = @"Server=localhost; uid=root; pwd=mysql; Database=tareacovid";
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            try
+            {
+                IEnumerable<Models.provincia> lista = null;
+
+                using (var db = new MySqlConnection(Conexion))
+                {
+                    var sql = "select provincias.nombre,vacunas.nombre,vacunados.fecha_vacunacion,pacientes.cedula,pacientes.nombre,pacientes.apellido,pacientes.telefono from(((provincias inner join vacunas on provincia_id = provincias.id) inner join vacunados on vacunas.id = vacunas.id) inner join pacientes on paciente_id = pacientes.id)";
+
+                    lista = db.Query<Models.provincia>(sql);
+
+                    Respuesta.ls = lista;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Respuesta.mensaje = ex.Message;
+            }
+
+
+            return Ok(Respuesta);
+        }
+
+        [HttpPost]
+
+        public IActionResult Insert(Models.provincia provincia)
+        {
+
+            try
+            {
+                using (var db = new MySqlConnection(Conexion))
+                {
+                    var sql = "insert into provincias(nombre)" +
+                        "values(@nombre)";
+
+                    Respuesta.exito = db.Execute(sql, provincia);
+                    Respuesta.mensaje = " 201 CREATED ";
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Respuesta.mensaje = "Error: 415 UNSUPPORTED MEDIA TYPE >> " + ex.Message;
+
+            }
+
+            return Ok(Respuesta.mensaje + Respuesta.exito);
+
+
+
+
+
+
+        }
+
+        [HttpPut]
+
+        public IActionResult Edit(Models.pacientes pacientes)
+        {
+
+            try
+            {
+                using (var db = new MySqlConnection(Conexion))
+                {
+                    var sql = "UPDATE pacientes SET cedula=@cedula, nombre=@nombre, apellido=@apellido,telefono=@telefono,fecha_nacimiento=@fecha_nacimiento" +
+                        " WHERE id=@id";
+
+                    Respuesta.exito = db.Execute(sql, pacientes);
+                    Respuesta.mensaje = " 201 CREATED ";
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Respuesta.mensaje = "Error: 415 UNSUPPORTED MEDIA TYPE >> " + ex.Message;
+
+            }
+
+            return Ok(Respuesta.mensaje + Respuesta.exito);
+
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Models.provincia provincia)
+        {
+            try
+            {
+                using (var db = new MySqlConnection(Conexion))
+                {
+                    var sql = "delete from provincia where id=@id";
+
+                    Respuesta.exito = db.Execute(sql, provincia);
+                    Respuesta.mensaje = " 201 CREATED ";
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Respuesta.mensaje = "Error: 415 UNSUPPORTED MEDIA TYPE >> " + ex.Message;
+
+            }
+
+            return Ok(Respuesta.mensaje + Respuesta.exito);
+
+
+
+        }
+
+    }
+}
---
+++ WebApi_Tarea7/Models/provincia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Tarea7.Models
+{
+    public class provincia
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+    }
+}
